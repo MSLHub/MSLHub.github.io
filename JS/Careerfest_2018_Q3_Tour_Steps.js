725c7af206a2a93036daa509f2c243e1002701af
MSLHub <matie.natov@sephora.com>
2018-05-17 14:49:09 -0700

    Update Careerfest_2018_Q3_Tour_Steps.js

--- JS/Careerfest_2018_Q3_Tour_Steps.js
+++ JS/Careerfest_2018_Q3_Tour_Steps.js
@@ -6,25 +6,49 @@ var tour = new Tour({
   {
     element: "#tour-launch",
 	placement: "bottom",
-    title: "Welcome to the Careerfest Registration Page!",
-    content: "Here, you can register for a Careerfest class. Or many! Press 'next' to see how."
+    title: "Welcome to the Careerfest!",
+    content: "Press 'next' to see page features."
   },
   {
     element: "#tour-days",
-	placement: "bottom",
+	placement: "top",
     title: "Choose a Day that works best for you",
     content: "Simply click on the day that works best for your schedule, and the available class cards will sort below."
   },
   {
     element: "#tour-categories",
-	placement: "bottom",
+	placement: "top",
     title: "Then, choose a Competency that you'd like to develop",
     content: "Hopefully, this filter can help you choose the best class to improve your skillset."
   },
   {
+    element: "#tour-card",
+	placement: "top",
+    title: "Here's a Class Card",
+    content: "It contains everything you need to register."
+  },
+  {
+    element: "#tour-category-bites",
+	placement: "top",
+    title: "Competencies this class develops",
+    content: "These tiny logos help you identify what class supports what competency. Some classes support more than one!"
+  },
+  {
+    element: "#tour-class-times",
+	placement: "top",
+    title: "Choose a Class Time",
+    content: "Once you've glazed over the information, click a button to select the best class time."
+  },
+  {
+    element: "#tour-take-class-survey",
+	placement: "top",
+    title: "After class is over, take survey!",
+    content: "Just a few minutes of you time can help improve future Careerfest classes."
+  },
+  {
     element: "#survey-link",
 	placement: "top",
-    title: "<span class='glyphicon glyphicon-thumbs-up'></span> Leave Feedback",
+    title: "<span class='glyphicon glyphicon-thumbs-up'></span> Leave Feedback for MSL",
     content: "Tell us what works and what could be improved."
   }
 ]});
