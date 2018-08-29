ea7ab19c18ad023fa2dfd34243c5789a6e506f1b
MSLHub <matie.natov@sephora.com>
2018-08-29 10:33:34 -0700

    Update Animation_Themes.js

--- JS/Animation_Themes.js
+++ JS/Animation_Themes.js
@@ -13,3 +13,20 @@ $('.close-help-me_BLACK').click(function(){
   $( "#help-content_BLACK" ).hide();
   $( "#help-widget_BLACK" ).removeClass("help-widgetOpen_BLACK");
 });
+
+
+
+// GET HELP BUTTON - RED THEME
+
+$( "#help-content_RED" ).hide();
+
+function myFunction() {
+ $( "#help-content_RED" ).show();
+ $( "#gethelp_RED" ).addClass("gethelpOpen_RED");
+ $( "#help-widget_RED" ).addClass("help-widgetOpen_RED");
+}
+$('.close-help-me_RED').click(function(){
+  $( "#gethelp_RED" ).removeClass("gethelpOpen_RED");
+  $( "#help-content_RED" ).hide();
+  $( "#help-widget_RED" ).removeClass("help-widgetOpen_RED");
+});
